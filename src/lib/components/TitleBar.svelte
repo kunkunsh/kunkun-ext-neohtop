8269d6f95a41df5fd31b175f26113b1692b6163c
Abdenasser <nasser.elidrissi065@gmail.com>
2025-02-26 18:20:03 +0000

    removed title bar animation

--- src/lib/components/TitleBar.svelte
+++ src/lib/components/TitleBar.svelte
@@ -5,7 +5,6 @@
   <div class="title">
     <img src="/32x32.png" alt="NeoHtop" class="app-icon" />
     <div class="neon">NeoHtop</div>
-    <div class="scanlines"></div>
   </div>
 </div>
 
@@ -35,79 +34,12 @@
     font-size: 14px;
     font-weight: bold;
     color: var(--text);
-    text-shadow:
-      0 0 5px var(--text),
-      0 0 10px var(--text),
-      0 0 20px var(--blue),
-      0 0 40px var(--blue),
-      0 0 80px var(--blue);
-    animation: flicker 3s infinite alternate;
-  }
-
-  @keyframes flicker {
-    0%,
-    19.999%,
-    22%,
-    62.999%,
-    64%,
-    64.999%,
-    70%,
-    100% {
-      opacity: 1;
-      text-shadow:
-        0 0 5px var(--text),
-        0 0 10px var(--text),
-        0 0 20px var(--blue),
-        0 0 40px var(--blue),
-        0 0 80px var(--blue);
-    }
-    20%,
-    21.999%,
-    63%,
-    63.999%,
-    65%,
-    69.999% {
-      opacity: 0.4;
-      text-shadow: none;
-    }
-  }
-
-  .scanlines {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(
-      to bottom,
-      transparent 50%,
-      rgba(0, 0, 0, 0.2) 51%
-    );
-    background-size: 100% 4px;
-    animation: scanlines 0.6s steps(40) infinite;
-    pointer-events: none;
-  }
-
-  @keyframes scanlines {
-    from {
-      transform: translateY(0);
-    }
-    to {
-      transform: translateY(2px);
-    }
-  }
-
-  @media screen and (prefers-reduced-motion: reduce) {
-    .neon,
-    .scanlines {
-      animation: none;
-    }
   }
 
   .app-icon {
-    width: 16px;
-    height: 16px;
-    margin-right: 8px;
+    width: 24px;
+    height: 24px;
+    margin-right: 4px;
     display: flex;
     align-items: center;
   }
